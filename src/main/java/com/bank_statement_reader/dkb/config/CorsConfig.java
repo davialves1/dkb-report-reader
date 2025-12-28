2f17ee9add312efcf9af2cfb18ead111c1b7ad1b
davialves1 <davi.alves.00@gmail.com>
2025-12-28 16:39:48 +0100

    created investment feature and flyway db

--- src/main/java/com/bank_statement_reader/dkb/config/CorsConfig.java
+++ src/main/java/com/bank_statement_reader/dkb/config/CorsConfig.java
@@ -14,12 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5173",
-                                "http://localhost:4173",
-                                "http://18.192.136.54",
-                                "http://bankanalytics.de",
-                                "https://bankanalytics.de")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
